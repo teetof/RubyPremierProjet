b4e5ced39956840adc3be49e32cc7499aad2044e
Christophe GABET <teetof1970@gmail.com>
2018-09-16 16:09:41 +0200

    Test IDE RubyMine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def premier
-    render html: "Bonjour"
+    render html: "Bonjour tout le monde"
   end
 end
